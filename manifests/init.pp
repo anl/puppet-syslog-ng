86886cff92b5464c6bbd6747ecf7af553d4171ac
Andrew Leonard <andy@hurricane-ridge.com>
2013-05-20 06:28:53 -0700

    Install package.

--- manifests/init.pp
+++ manifests/init.pp
@@ -24,5 +24,6 @@
 #
 class syslogng inherits syslogng::params {
 
+  package { $syslogng::params::package: ensure => present }
 
 }
---
+++ manifests/params.pp
@@ -0,0 +1,26 @@
+# == Class: syslogng::params
+#
+# Params class for syslogng.
+#
+# === Examples
+#
+# class syslogng inherits syslogng::params {}
+#
+# === Authors
+#
+# Andrew Leonard
+#
+# === Copyright
+#
+# Copyright 2012 Andrew Leonard
+#
+class syslogng::params {
+  case $::operatingsystem {
+    ubuntu: {
+      $package = 'syslog-ng'
+    }
+    default: {
+      fail("Unsupported os: ${::operatingsystem}")
+    }
+  }
+}
